ea48c7accf8bed143956f45af37550ca913bc8c0
migruedaunam <migrueda@comunidad.unam.mx>
2024-07-23 22:57:22 -0600

    update functions

---
+++ new_proyect/JS/app.js
@@ -0,0 +1,6 @@
+let text = document.querySelector('.texto');  //.texto es = h1 en el ejemplo
+text.innerHTML = 'Ingrese el texto aqui';  //text es la variable de js
+
+function funcion1(){
+    alert ("Hola");
+}
